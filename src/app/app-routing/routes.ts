3c8f19a6e41bffc2473a0d1b3c3697e51b02bbf8
Avinash Tingre <tingre.avinash@gmail.com>
2021-03-25 12:10:57 +0530

    test commit

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,8 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes =[
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
-    {path: '', redirectTo: '/home', pathMatch: 'full'}
+    {path: 'contactus', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: AboutComponent}
 
 ];
